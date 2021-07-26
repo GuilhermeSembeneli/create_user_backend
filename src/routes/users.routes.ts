e1764aef5a03ce23ae4ab714a2960d532522cb75
Sembenevi <guilherme.sembeneli@gmail.com>
2021-07-26 16:45:00 -0300

    add middleware jwt

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,15 +1,16 @@
 import { Router } from 'express';
 import { userController } from '../modules/users';
+import { VerifyJWT } from '../modules/users/controller/VerifyJWT';
 
 
 const usersRoutes = Router();
 
 
-usersRoutes.get('/:id', (request, response) => {
+usersRoutes.get('/:id', VerifyJWT, (request, response) => {
     userController.findById(request, response);
 })
 
-usersRoutes.get('/', (request, response) => {
+usersRoutes.get('/', VerifyJWT, (request, response) => {
     userController.findAll(request, response);
 })
 
@@ -17,11 +18,16 @@ usersRoutes.post('/', (request, response) => {
     userController.create(request, response);
 })
 
-usersRoutes.put('/', (request, response) => {
+usersRoutes.post('/login', (request, response) => {
+    userController.login(request, response);
+})
+
+
+usersRoutes.put('/', VerifyJWT, (request, response) => {
     userController.update(request, response);
 })
 
-usersRoutes.delete('/:user_id', (request, response) => {
+usersRoutes.delete('/:user_id', VerifyJWT, (request, response) => {
     userController.delete(request, response);
 })
 
