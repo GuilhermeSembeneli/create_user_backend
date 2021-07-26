cbb0b5c0ee97a2d0397f11fbdb0c484fdd5658ee
Sembenevi <guilherme.sembeneli@gmail.com>
2021-07-26 16:43:50 -0300

    add new validators, password etc

--- src/modules/users/controller/index.ts
+++ src/modules/users/controller/index.ts
@@ -19,12 +19,15 @@ export class UserController {
     }
 
     async create(request: Request, response: Response) {
-        const { username } = request.body;
+        const { username, password } = request.body;
 
         const validator = this.UserService.validator('username', request.body);
+        const validatorPassword = this.UserService.validator('password', request.body);
+        
         if (validator.length) return response.status(400).json(validator);
+        if (validatorPassword.length) return response.status(400).json(validatorPassword);
 
-        const result = await this.UserRepository.create(username);
+        const result = await this.UserRepository.create({username, password});
 
         const submitValidator = this.UserService.responseValidator(result);
 
@@ -32,12 +35,12 @@ export class UserController {
     }
 
     async update(request: Request, response: Response) {
-        const { username, user_id } = request.body;
+        const { username, user_id, password } = request.body;
 
         const validator = this.UserService.validator('username', request.body);
         if (validator.length) return response.status(400).json(validator);
 
-        const update = await this.UserRepository.update({user_id, username});
+        const update = await this.UserRepository.update({user_id, username, password});
 
         const submitValidator = this.UserService.responseValidator(update);
        
@@ -52,6 +55,21 @@ export class UserController {
         const deleted = await this.UserRepository.delete(user_id);
 
         const submitValidator = this.UserService.responseValidator(deleted);
+
+        return response.status(submitValidator.status).json(submitValidator.message);
+    }
+
+    async login(request: Request, response: Response) {
+        const { password, username } = request.body;
+
+        const validator = this.UserService.validator('username', request.body);
+        if (validator.length) return response.status(400).json(validator);
+
+        const login = await this.UserRepository.signIn({password, username});
+
+        if (typeof login === 'object') return response.status(200).json(login);
+
+        const submitValidator = this.UserService.responseValidator(login);
 
         return response.status(submitValidator.status).json(submitValidator.message);
     }
