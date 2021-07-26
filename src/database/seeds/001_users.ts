4c26e38bdf98afebb12a852cd1915a3e75b66845
Sembenevi <guilherme.sembeneli@gmail.com>
2021-07-26 16:43:27 -0300

    add new fields in seeders

--- src/database/seeds/001_users.ts
+++ src/database/seeds/001_users.ts
@@ -7,6 +7,6 @@ export async function seed(knex: Knex): Promise<void> {
 
     // Inserts seed entries
     await knex("users").insert([
-        { username: "Guilherme Sembeneli", user_id: uuid()},
+        { username: "Guilherme Sembeneli", user_id: uuid(), password: 'blabla'},
     ]);
 };
